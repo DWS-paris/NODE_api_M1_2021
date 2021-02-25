b33066a553d9bf0bd19a9f5067acb72b1a747e62
Julien Noyer <j.noyer@digitalworkshop.fr>
2021-02-25 15:57:49 +0100

    Mise en place du CRUD api

--- router/api.router.js
+++ router/api.router.js
@@ -15,15 +15,72 @@ Routes definition
         }
 
         routes(){
-            // API route definition
+            // Main API route definition
             this.router.get('/', ( req, res ) => {
-                // Return JSON data
+                // TODO: add the API definition
                 return res.json( {
+                    url: req.originalUrl,
+                    method: 'GET',
                     msg: 'API Home page',
                     data: null,
                     err: null
                 });
             });
+
+            // CRUD: route to Create data
+            this.router.post('/page', ( req, res ) => {
+                return res.json( {
+                    url: req.originalUrl,
+                    method: 'POST',
+                    msg: 'Data created',
+                    data: null,
+                    err: null
+                });
+            })
+
+            // CRUD: route to Read all data
+            this.router.get('/page', ( req, res ) => {
+                return res.json( {
+                    url: req.originalUrl,
+                    method: 'GET',
+                    msg: 'Data send',
+                    data: null,
+                    err: null
+                });
+            })
+
+            // CRUD: route to Read one data
+            this.router.get('/page/:id', ( req, res ) => {
+                return res.json( {
+                    url: req.originalUrl,
+                    method: 'GET',
+                    msg: 'Data send',
+                    data: null,
+                    err: null
+                });
+            })
+
+            // CRUD: route to Update one data
+            this.router.put('/page/:id', ( req, res ) => {
+                return res.json( {
+                    url: req.originalUrl,
+                    method: 'PUT',
+                    msg: 'Data updated',
+                    data: null,
+                    err: null
+                });
+            })
+
+            // CRUD: route to Delete one data
+            this.router.delete('/page/:id', ( req, res ) => {
+                return res.json( {
+                    url: req.originalUrl,
+                    method: 'DELETE',
+                    msg: 'Data deleted',
+                    data: null,
+                    err: null
+                });
+            })
         }
 
         init(){
